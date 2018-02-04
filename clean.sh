bdf832d2a43502cfce59deaec2901c28d567f08c
Anton Samarchyan <desecho@gmail.com>
2018-02-04 17:51:54 -0500

    Add comments to clean script

--- clean.sh
+++ clean.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# We want to remove imports before running isort.
 autoflake --remove-all-unused-imports --in-place -r django_tqdm
 yapf -ri django_tqdm
+# We want to run isort after yapf to make sure isort lint pass.
 isort -rc django_tqdm
