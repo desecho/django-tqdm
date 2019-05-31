947549ec419d1bd0cf741dbce593d3659c5426b4
Anton Samarchyan <desecho@gmail.com>
2019-05-30 21:47:01 -0400

    Add Python 3.7 to the list of supported versions of Python

--- setup.py
+++ setup.py
@@ -39,6 +39,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Environment :: Console',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Software Development :: User Interfaces',
