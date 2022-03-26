ae9a0b8659343923a01a353baeace97a865e64c9
Anton Samarchyan <desecho@gmail.com>
2022-03-26 16:11:41 -0400

    Remove python 3.5 and 3.6 support

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     install_requires=requirements(),
     packages=find_packages(),
     zip_safe=False,
-    python_requires=">=3.5",
+    python_requires=">=3.7",
     include_package_data=True,
     platforms="any",
     classifiers=[
@@ -37,8 +37,6 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
