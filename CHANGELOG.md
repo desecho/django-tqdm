4613e62c169b2a065c26301a49b3aa1cffe436d5
Anton Samarchyan <desecho@gmail.com>
2022-03-26 17:41:59 -0400

    Update version to 0.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,8 @@
 
 ## Unreleased
 
+## 1.1.0
+
 - Add Python 3.8, 3.9, 3.10 support
 - Remove support for Python 2.7, 3.4, 3.5, 3.6 Django < 2.2
 
--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def requirements():
 
 setup(
     name="django_tqdm",
-    version="1.0.0",
+    version="1.1.0",
     url="https://github.com/desecho/django-tqdm",
     project_urls={"Changelog": "https://github.com/desecho/django-tqdm/blob/master/CHANGELOG.md"},
     license="MIT",
