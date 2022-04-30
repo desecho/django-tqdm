8fb8aba772858cb5be033e35d3f8e3ced16bf8c4
Anton Samarchyan <desecho@gmail.com>
2022-04-30 18:38:33 -0400

    Fix linter config

--- scripts/pydiatra.sh
+++ scripts/pydiatra.sh
@@ -2,7 +2,7 @@
 
 set -eou pipefail
 
-result=$(find django_tqdm -name '*.py' -exec py3diatra {} \;)
+result=$(find django_tqdm tests -name '*.py' -exec py3diatra {} \;)
 if [[ $result ]]; then
     echo "$result"
     exit 1
