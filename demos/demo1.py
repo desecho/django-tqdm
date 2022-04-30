4fa8ab1ea1152843af84cf90a5318478dd37c74d
Anton Samarchyan <desecho@gmail.com>
2022-04-30 18:38:19 -0400

    Add annotations for demos

--- demos/demo1.py
+++ demos/demo1.py
@@ -1,11 +1,13 @@
 """Demo 1 - Simple usage."""
 
+from typing import Any
+
 from django_tqdm import BaseCommand
 
 
 class Command(BaseCommand):
-    def handle(self, *args, **options):
-        def demo():
+    def handle(self, *args: Any, **options: Any) -> None:
+        def demo() -> None:  # pylint: disable=duplicate-code
             self.info("info")
             self.error("error")
 
--- demos/demo2.py
+++ demos/demo2.py
@@ -1,13 +1,14 @@
 """Demo 2 - tqdm usage."""
 
 from time import sleep
+from typing import Any
 
 from django_tqdm import BaseCommand
 
 
 class Command(BaseCommand):
-    def handle(self, *args, **options):
-        def demo():
+    def handle(self, *args: Any, **options: Any) -> None:
+        def demo() -> None:
             t = self.tqdm(total=50)
             for x in range(50):
                 sleep(0.02)
